7ffe247916d0c646e941dc18a09101562b6cc16e
rustydev19 <rusty@findgaddi.com>
2015-06-29 01:54:18 +0530

    contributing from official account
    contributing from official account

--- ui/footer.php
+++ ui/footer.php
@@ -3,3 +3,5 @@
 			&copy; Copyright 2015 FindGaddi.com | Powered By Enroute Logistics Pvt Ltd. | About | Terms and Conditions | Contacts Us | <a href="#">Top</a>
 	</small>
 </div>
+
+//contributing from official account
