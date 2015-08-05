f38ab445cf577ccd42f8d7c5bf9be7172c23b767
rustydev1909 <rusty@findgaddi.com>
2015-08-05 21:07:58 +0530

    notifications not loading on dashboard
    APIs updated

--- ui/user/notification.php
+++ ui/user/notification.php
@@ -2,37 +2,27 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
-	$id = "";
-	if(isset($_POST['id'])){
-		$id = $_POST['id'];
-	}
-
 	require_once '../../framework/Vehicle.php';
 	require_once '../../framework/Notification.php';
 	
+	$mUser = new User();
+	$companyId = $mUser->getCompany();
+	$mCompany = new Company($companyId);
+	
 	$mNotiResourceList = array();
-	if($id == ""){
-		$mNotificationList = Vehicle::getAllNotifications();
-		//echo sizeof($mNotificationList);
-		for($i=0;$i<sizeof($mNotificationList);$i++){
-			$noti = new Notification($mNotificationList[$i]);
-			$temp = $noti->getResource();
-			if($temp['priority'] != 1)
-				array_push($mNotiResourceList, $temp);
-			
-			//echo $temp['string']."<br>";
-		}
-	} else{
-		$mNotificationList = Vehicle::getAllNotifications();
-		//echo sizeof($mNotificationList);
-		for($i=0;$i<sizeof($mNotificationList);$i++){
-			$noti = new Notification($mNotificationList[$i]);
-			$temp = $noti->getResource();
+
+	if($mCompany != null)
+		$mNotificationList = $mCompany->getAllNotifications();
+	else
+		$mNotificationList = $mUser->getAllNotifications();
+	//echo sizeof($mNotificationList);
+	for($i=0;$i<sizeof($mNotificationList);$i++){
+		$noti = new Notification($mNotificationList[$i]);
+		$temp = $noti->getResource();
+		if($temp['priority'] != 1)
 			array_push($mNotiResourceList, $temp);
-			
-			//echo $temp['string']."<br>";
-		}
+		
+		//echo $temp['string']."<br>";
 	}
 	echo json_encode($mNotiResourceList);
 ?>
