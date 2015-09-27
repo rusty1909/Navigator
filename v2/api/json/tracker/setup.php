936cbe706d5304a3cfbe4f2d7dab3668873f1007
rustydev1909 <rusty@findgaddi.com>
2015-09-27 18:38:07 +0530

    basic v2 setup
    basic v2 setup

---
+++ v2/api/json/tracker/setup.php
@@ -0,0 +1,36 @@
+<?php
+/*success : 
+{
+    status : "SUCCESS"
+    request : "setup"
+    result : {
+        vehicle : {
+            id : "142763"
+            type : "Truck"
+            number : "DL12T3426"
+            year : "2012"
+            date_added : "15-06-2015"
+        }
+        company : {
+            id : "4725"
+            name : "Enroute India Pvt Ltd"
+            phone : "8756345472"
+            admin : {
+                id : "2648"
+                name : "Prateek Srivastava"
+                phone : "8354627345"
+            }
+        }
+    }
+} */
+
+/*fail : 
+{
+    status : "FAILURE"
+    request : "setup"
+    result : {
+        reason : "FAIL_VEHICLE"
+        message : "No such vehicle available!!!"
+    }
+}*/
+?>
