da265c3ff295fb472396080bec7df1afb4119724
rustydev1909 <rusty@findgaddi.com>
2015-09-27 19:48:27 +0530

    adding setup support for tracker
    adding setup support for tracker

--- v2/api/json/tracker/setup.php
+++ v2/api/json/tracker/setup.php
@@ -29,8 +29,61 @@
     status : "FAILURE"
     request : "setup"
     result : {
-        reason : "FAIL_VEHICLE"
-        message : "No such vehicle available!!!"
+		error : {
+			reason : "FAIL_VEHICLE"
+			message : "No such vehicle available!!!"
+		}
     }
 }*/
+
+private class SetupResponse {
+	public $status;
+	public $request = "setup";
+	public $result;
+}
+
+private class Error {
+	public $reason;
+	public $message;
+}
+
+private class Vehicle {
+	public $id;
+	public $number;
+	public $type;
+	public $year;
+	public $date_added;
+}
+
+private class Company {
+	public $id;
+	public $name;
+	public $phone;
+	public $admin;
+}
+
+private class Admin {
+	public $id;
+	public $name;
+	public $phone;
+}
+
+$setupResponse = new SetupResponse();
+
+if(isset($_GET['vehicle']) && isset($_GET['type']) && isset($_GET['imei']) && isset($_GET['mac'])) {
+	$vehicle = trim($_GET['vehicle']);
+	$type = trim($_GET['type']);
+	$imei = trim($_GET['imei']);
+	$mac = trim($_GET['mac']);
+} else {
+	$setupResponse->status = "FAILURE";
+	$setupResponse->request = "setup";
+	
+	$error = new Error();
+	$error->reason = "INSUFFICIENT DATA";
+	$error->message = "Please provide complete information."
+	
+	$setupResponse->error = $error;
+	echo json_encode($setupResponse);
+}
 ?>
